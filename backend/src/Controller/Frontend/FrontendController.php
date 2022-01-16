fa4f95ad93a8513ea25ca75bf554c4e128bf157e
JohnTheBoss <git@janoskollar.hu>
2022-01-16 23:51:09 +0100

    add css mime type

--- backend/src/Controller/Frontend/FrontendController.php
+++ backend/src/Controller/Frontend/FrontendController.php
@@ -34,40 +34,20 @@ class FrontendController extends AbstractController
     private function getFile($path)
     {
         $filename = __DIR__ . "/../../../public/frontend/{$path}";
-        $fileContent = file_get_contents($filename);
-        /*
-        $file = new File($filename);
-
-        return $this->file($file);
-*/
-        /*
-
-                $fileContent = file_get_contents($filename);
-
-                $response = new Response($fileContent);
-
-                // Create the disposition of the file
-                $disposition = $response->headers->makeDisposition(
-                    ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-                    $path
-                );
-
-                // Set the content disposition
-                $response->headers->set('Content-Disposition', $disposition);
-
-                // Dispatch request
-                return $response;
-
-        */
-
 
         if (file_exists($filename)) {
+            $fileContent = file_get_contents($filename);
             $response = new Response($fileContent);
             $fileType = mime_content_type($filename);
 
             $path_parts = pathinfo($filename);
             if ($path_parts['extension'] === 'js') {
                 $fileType = 'application/javascript';
+            }
+
+            $path_parts = pathinfo($filename);
+            if ($path_parts['extension'] === 'css') {
+                $fileType = 'text/css';
             }
 
             $response->headers->set('Content-Type', $fileType);
